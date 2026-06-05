cc2a03439750bd935e058cdd0dfed22c698f7a80
Jonathan Biemond <jonbiemond@gmail.com>
2026-06-04 17:28:03 -0700

    Disable <Tab> key for completions

--- init.lua
+++ init.lua
@@ -957,6 +957,9 @@ do
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
       preset = 'default',
 
+      -- disable keymap from preset
+      ['<Tab>'] = {},
+
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
