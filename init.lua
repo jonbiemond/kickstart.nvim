40a1a7492c9fc9c49ba6c3184131f2e8c2721b58
Jonathan Biemond <jonbiemond@gmail.com>
2026-06-21 13:16:26 -0700

    Include hidden files in search

--- init.lua
+++ init.lua
@@ -510,7 +510,9 @@ do
         '%__pycache__/',
       },
     },
-    -- pickers = {}
+    pickers = {
+      find_files = { hidden = true },
+    },
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
     },
